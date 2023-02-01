d8162b4a53eabc109a86879539562c2cee20e4bd
Emerson Rafael <emersona7x@hotmail.com>
2023-02-01 04:36:23 -0300

    [CHG] CHANGE MAIN TO EN

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from random import choice
 
 from MODEL.image2text import execute_ocr
 
-# DEFININDO O DIRETÓRIO CONTENDO IMAGES PARA TESTE
+# SETTING THE DIRECTORY WITH IMAGES TO TEST
 dir_images = "TESTS/IMAGES/training_data/images"
 
-# DEFININDO OS TIPOS DE ARQUIVOS DESEJADOS
+# SETTING EXTENSIONS ACCEPTED
 types_accepted = (".png", ".jpg")
 
-# OBTENDO TODAS AS IMAGENS
+# GETTING ALL IMAGES
 list_all_images = [str(file) for file in Path(dir_images).absolute().iterdir() if file.suffix in types_accepted]
 
-# OBTENDO UMA IMAGEM DE FORMA ALEATÓRIA
+# RANDOM AN IMAGE AMONG ALL
 image = choice(list_all_images)
 
 print("SELECTED IMAGE: {}".format(image))
 
-# SOLICITANDO O OCR
+# REQUEST OCR
 result_ocr = execute_ocr(image_input=image, view_image=True)
 
 print(result_ocr)
